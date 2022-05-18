8aa62a8413eb656b1caf720f217b1d71c46488e2
OwenLobato <owenlobatov@gmail.com>
2022-05-17 19:09:53 -0500

    Check logic of the results
    Co-authored-by: acorzo alexecorzo@gmail.com
    Co-authored-by: EdwardMD @Edwardmi444@gmail.com

--- src/bowling.rb
+++ src/bowling.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
+require_relative 'frames'
 
 # initial class
 class BowlingGame
-  def welcome
+  
+  def self.welcome
     'Bienvenido a nuestro bowling game'
   end
+
 end
--- src/bowling_spec.rb
+++ src/bowling_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-
 require_relative 'bowling'
+
 describe BowlingGame do
   describe '#newgame' do
     it 'printwelcome' do

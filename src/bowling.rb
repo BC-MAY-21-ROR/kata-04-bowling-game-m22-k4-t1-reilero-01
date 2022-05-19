d67262ed3dd7794568a00a1ca6fb55fced0fcb78
EdwardMD <Edwardmi444@gmail.com>
2022-05-19 16:06:40 -0500

    results printing
    The "rolls" and "score" methods were modified by adding fixed values in order to perform basic tests with the game performance.
    
    Co-authored-by: @OwenLobato <owenlobatov@gmail.com>
    Co-authored-by: @acorzo <alexecorzo@gmail.com>

--- src/bowling.rb
+++ src/bowling.rb
@@ -2,10 +2,9 @@
 
 # initial class
 class BowlingGame
-  
   def initialize
-    @rolls = []
-    @score = 0
+    @rolls = [1, 4, 4, 5, 6, 4, 5, 5, 10, nil, 0, 1, 7, 3, 6, 4, 10, nil, 2, 8]
+    @score = [5, 14, 29, 49, 60, 61, 77, 97, 117, 133]
   end
 
   def roll(pins)
@@ -14,17 +13,17 @@ class BowlingGame
 
   def score
     result = 0
-    index=0
+    index = 0
     10.times do
-      if is_strike?(index) 
+      if is_strike?(index)
         result += strike_score(index)
-        index+=1
+        index += 1
       elsif is_spare?(index)
         result += spare_score(index)
-        index+=2
+        index += 2
       else
         result += frame_score(index)
-        index+=2
+        index += 2
       end
     end
     result
@@ -35,11 +34,11 @@ class BowlingGame
   end
 
   def strike_score(i)
-    10 + @rolls[i+1] + @rolls[i+2]
+    10 + @rolls[i + 1] + @rolls[i + 2]
   end
 
   def is_spare?(i)
-    (@rolls[i] + @rolls[i+1]) == 10
+    (@rolls[i] + @rolls[i + 1]) == 10
   end
 
   def spare_score(i)
@@ -47,7 +46,22 @@ class BowlingGame
   end
 
   def frame_score(i)
-    @rolls[i]+@rolls[i+1]
+    @rolls[i] + @rolls[i + 1]
   end
 
+  def print_score
+    l = @rolls.length / 2
+    index = 0
+    l.times do
+      print "[#{@rolls[index]},#{@rolls[index + 1]}]"
+      index += 2
+    end
+    print "\n"
+    l.times do |i|
+      print "[ #{@score[i]} ]"
+    end
+  end
 end
+
+x = BowlingGame.new
+x.print_score

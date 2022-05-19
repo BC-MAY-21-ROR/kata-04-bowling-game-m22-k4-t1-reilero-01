52cea77c6b9d6f496b87e30a01d877e396f29cd5
OwenLobato <owenlobatov@gmail.com>
2022-05-18 19:08:33 -0500

    Create spare and strike rules
    Co-authored-by: acorzo alexecorzo@gmail.com
    Co-authored-by: EdwardMD @Edwardmi444@gmail.com

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,30 +1,44 @@
 # frozen_string_literal: true
 require 'rspec'
 require_relative '../src/bowling'
-#require_relative  '/src/frame'
 
 describe BowlingGame do
-  describe '#newgame' do
-    it 'printwelcome' do
-      @bowl = BowlingGame.new
-      expect(@bowl.welcome).to eq('Bienvenido a nuestro bowling game')
-    end
 
     it 'can create game' do
-      @bowl = BowlingGame.new
-      expect(@bowl.score).to eq(0)
+      bowl = BowlingGame.new
     end  
 
     it 'no pin game' do
-      @bowl = BowlingGame.new
-      1.times{@bowl.roll(0)}
-      expect(@bowl.score).to eq(0)
+      bowl = BowlingGame.new
+      20.times{bowl.roll(0)}
+      expect(bowl.score).to eq(0)
     end
+
+    it 'only 1 pin game' do
+      bowl = BowlingGame.new
+      20.times{bowl.roll(1)}
+      expect(bowl.score).to eq(20)
+    end
+
+    it 'roll a spare' do
+      bowl = BowlingGame.new
+      bowl.roll(8)
+      bowl.roll(2)
+      bowl.roll(1)
+      17.times{bowl.roll 0}
+      expect(bowl.score).to eq(12)
+    end
+
+    it 'roll a strike' do
+      bowl = BowlingGame.new
+      bowl.roll(10)
+      bowl.roll(3)
+      bowl.roll(2)
+      16.times{bowl.roll 0}
+      expect(bowl.score).to eq(20)
+    end
+
     
-      # it 'perfect game' 
-      #   12.times{@game.roll(10)}
-      #   expect(@game.score).to eq(300)
-     
+
     
-  end
 end
--- src/bowling.rb
+++ src/bowling.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
-require_relative '../src/frame'
 
 # initial class
 class BowlingGame
- 
   
   def initialize
     @rolls = []
@@ -11,23 +9,45 @@ class BowlingGame
   end
 
   def roll(pins)
-     @rolls << 0
+    @rolls << pins
   end
 
   def score
-    result = @rolls.sum
+    result = 0
+    index=0
+    10.times do
+      if is_strike?(index) 
+        result += strike_score(index)
+        index+=1
+      elsif is_spare?(index)
+        result += spare_score(index)
+        index+=2
+      else
+        result += frame_score(index)
+        index+=2
+      end
+    end
+    result
   end
 
-  def welcome
-    'Bienvenido a nuestro bowling game'
+  def is_strike?(i)
+    @rolls[i] == 10
+  end
+
+  def strike_score(i)
+    10 + @rolls[i+1] + @rolls[i+2]
+  end
+
+  def is_spare?(i)
+    (@rolls[i] + @rolls[i+1]) == 10
+  end
+
+  def spare_score(i)
+    10 + @rolls[i + 2]
+  end
+
+  def frame_score(i)
+    @rolls[i]+@rolls[i+1]
   end
 
 end
-
-# b = BowlingGame.new
-# b.roll(3)
-# puts b.score
-
-
-# @rolls = [3,5].sum  
-# 8
